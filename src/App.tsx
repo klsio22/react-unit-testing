f8196763fdd83ff95feb8bb607b03f0fab60b683
Klesio <klesio.dev@gmail.com>
2022-02-18 21:04:46 -0300

    started tests in react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Vamos lá
+    </div>
+  )
+}
+
+export default App
