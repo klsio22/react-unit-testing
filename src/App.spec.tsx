502b1c41dfa2bca7a9640d3c25fdce333cfa1103
Klesio <klesio.dev@gmail.com>
2022-02-26 15:42:53 -0300

    Finished tests live and init error rerender

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,20 +1,33 @@
-import { render, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import {
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+  screen,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-
-import App from './App';
+import List from './components/List';
 
 describe('App componet', () => {
-  it('should render list item', () => {
-    const { getByText } = render(<App />);
+  it('should render list item', async () => {
+    const { getByText, rerender } = render(
+      <List initialItems={['Diego', 'Rodz', 'Mayk', 'Klesio']} />
+    );
     expect(getByText('Diego')).toBeInTheDocument();
     expect(getByText('Rodz')).toBeInTheDocument();
     expect(getByText('Mayk')).toBeInTheDocument();
     expect(getByText('Klesio')).toBeInTheDocument();
+
+    rerender(<List initialItems={['Julia']} />);
+
+    expect(screen.getByText('Julia')).toBeInTheDocument();
+    expect(screen.queryByText('Mayk')).not.toBeInTheDocument();
   });
 
   it('should be able to add new item to the list', async () => {
-    const { getByText, getByPlaceholderText } = render(<App />);
+    const { getByText, getByPlaceholderText } = render(
+      <List initialItems={['']} />
+    );
 
     const inputElement = getByPlaceholderText('Novo item');
     const addButton = getByText('Adicionar');
@@ -22,24 +35,23 @@ describe('App componet', () => {
     userEvent.type(inputElement, 'Novo');
     userEvent.click(addButton);
 
-    await waitFor(()=>{
+    await waitFor(() => {
       expect(getByText('Novo')).toBeInTheDocument();
-    })
-
+    });
   });
 
   it('should be able to remove new item to the list', async () => {
-    const { getByText, getAllByText } = render(<App />);
+    const { getByText, getAllByText } = render(
+      <List initialItems={['Diego']} />
+    );
 
     const addButton = getByText('Adicionar');
-    const removeButons = getAllByText('Remover')
-   
+    const removeButons = getAllByText('Remover');
+
     userEvent.click(removeButons[0]);
 
-    await waitForElementToBeRemoved(()=>{
-      return getByText('Diego')
-    })
-
+    await waitForElementToBeRemoved(() => {
+      return getByText('Diego');
+    });
   });
-
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
-import { useState } from 'react';
+import List from './components/List';
 
 function App() {
-  const [newItem, setNewItem] = useState('');
-  const [list, setList] = useState(['Diego', 'Rodz', 'Mayk', 'Klesio']);
-
-  function addToList() {
-    setTimeout(() => {
-      setList((state) => [...state, newItem]);
-    }, 500);
-  }
-
-  function removeFromList(item : string) {
-    setTimeout(() => {
-      setList(state => state.filter(item => item !== item));
-    }, 500);
-  }
-
-  return (
-    <>
-    <input placeholder='Novo item' value={newItem} onChange={e => setNewItem (e.target.value)} />
-      <button onClick={addToList}>Adicionar</button>
-
-      <ul>
-        {list.map((item) => (
-          <li key={item}>
-            {item}
-            <button onClick={() => removeFromList(item)}>Remover</button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <List initialItems={['Diego', 'Rodz', 'Mayk', 'Klesio']} />;
 }
 
 export default App;
